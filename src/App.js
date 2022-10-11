e7cc1ebc2e42a1d64a1e2a4ab1a2a5cfb56dbf3a
franchoro <103784840+franchoro@users.noreply.github.com>
2022-10-11 09:42:43 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import './App.css';
+import PayPal from "./components/PayPal";
+
+function App() {
+
+  const [checkout, setCheckout] = useState(false)
+  return (
+    <div className="App">
+
+
+      {checkout ? (
+        <PayPal />
+      ) :(
+      <button onClick={()=>{setCheckout(true);
+      }}
+      >
+         Checkout
+         </button>
+      )}
+    </div>
+  );
+}
+
+export default App;
