ae713cea4cc222a774a620599f39166f6350f0a1
franchoro <103784840+franchoro@users.noreply.github.com>
2022-12-04 21:28:20 -0500

    Add files via upload

---
+++ src/components/caja.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/Carousel.css';
+
+function Caja() {
+    return(
+        
+    <Fragment>
+
+        <textarea placeholder="Que esta pasando?"/>
+        <button>Twitear</button>
+    </Fragment>
+    );
+}
+
+/*
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Caja/>,rootElement); */
+
+export default Caja;
